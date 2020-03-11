5c3870855d6fb3e0d8e74f03a93eca213e20cfba
wxy <979286092@qq.com>
2020-03-11 18:54:26 +0800

    连接数据库并存储历史数据

--- data/getData.py
+++ data/getData.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import time
+import pymysql
+import traceback
 
 
 def get_data():
@@ -49,10 +51,65 @@ def get_data():
             heal = city_infos["total"]["heal"]
             dead = city_infos["total"]["dead"]
             details.append([update_time, province, city, confirm, confirm_add, heal, dead])
-    print(history)
-    print("------------------------------------")
-    print(details)
-    #return history, details
+    return history, details
 
 
-get_data()
+def get_conn():
+    conn = pymysql.connect(host="localhost",
+                           user="root",
+                           password="misswang",
+                           db="cov",
+                           charset="utf8"
+                           )
+    cursor = conn.cursor()
+    return conn, cursor
+
+
+def close_conn(conn, cursor):
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
+
+
+def insert_history():
+    cursor = None
+    conn = None
+    try:
+        dic = get_data()[0]  # 0是历史数据字典，1是最新详细数据
+        print(f"{time.asctime()}开始插入数据")
+        conn, cursor = get_conn()
+        sql = "insert into history values(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        for k, v in dic.items():
+            cursor.execute(sql, [k, v.get("confirm"), v.get("confirm_add"), v.get("suspect"),
+                                 v.get("suspect_add"), v. get("heal"), v.get("heal_add"),
+                                 v.get("dead"), v.get("dead_add")])
+        conn.commit()
+        print(f"{time.asctime()}插入历史数据完毕")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
+
+
+def update_history():
+    cursor = None
+    conn = None
+    try:
+        dic = get_data()[0]  # 0是历史数据字典，1是最新详细数据
+        print(f"{time.asctime()}开始插入数据")
+        conn, cursor = get_conn()
+        sql = "insert into history values(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        for k, v in dic.items():
+            cursor.execute(sql, [k, v.get("confirm"), v.get("confirm_add"), v.get("suspect"),
+                                 v.get("suspect_add"), v.get("heal"), v.get("heal_add"),
+                                 v.get("dead"), v.get("dead.add")])
+        conn.commit()
+        print(f"{time.asctime()}插入历史数据完毕")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
+
+
+insert_history()
